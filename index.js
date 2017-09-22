95dd05be972d21d65ff926649678512ff20a72ea
Matt Priour <mpriour@gmail.com>
2017-09-21 22:30:04 -0500

    minimum working example using List class

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ import "./node_modules/d3/build/d3.js";
 import "./node_modules/venn.js/venn.js";
 
 import {readJSON} from "./lib/helpers.js";
+import List from "./lib/List.js"
 
 fetch("data/lists.json").then(readJSON).then(data => {
-    let listsReqs = data.lists.map( list => fetch("data/" + list.slug + ".json").then(readJSON) );
-    Promise.all(listsReqs).then(results => console.log(results));
+    /* let listsReqs = data.lists.map( list => fetch("data/" + list.slug + ".json").then(readJSON) );
+    Promise.all(listsReqs).then(results => console.log(results)); */
+    let lists = data.lists.map( list => {
+        return new List({
+            handle: "mattpriour",
+            slug: list.slug,
+            path: "data/" + list.slug + ".json"
+        });
+    } );
+    Promise.all(lists.map( li => li.loaded )).then(results => console.log(results));
 });
