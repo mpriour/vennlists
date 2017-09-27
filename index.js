f0fed7f87af16593d0e9b35bb628baea292cb3bb
Matt Priour <mpriour@gmail.com>
2017-09-26 23:02:21 -0700

    use ListCollection and add mouseover venn group count

--- index.js
+++ index.js
@@ -2,30 +2,63 @@
 import "./node_modules/d3/build/d3.js";
 import "./node_modules/venn.js/venn.js";
 
-import {readJSON} from "./lib/helpers.js";
-import List from "./lib/List.js"
+import {readJSON, getCombinations} from "./lib/helpers.js";
+import List from "./lib/List.js";
+import ListCollection from "./lib/ListCollection.js";
 
-let lists;
 fetch("data/lists.json").then(readJSON).then(data => {
-    /* let listsReqs = data.lists.map( list => fetch("data/" + list.slug + ".json").then(readJSON) );
-    Promise.all(listsReqs).then(results => console.log(results)); */
-    lists = data.lists.map( list => {
-        return new List({
-            handle: "mattpriour",
-            slug: list.slug,
-            path: "data/" + list.slug + ".json"
-        });
-    } );
-    Promise.all(lists.map( li => li.loaded )).then(results => {
-        console.log(results);
-        const l1 = results[0];
-        const users = l1.users;
-        const handles = l1.handles;
-        const userArray = l1.asArray();
-        const ids = l1.ids;
-        console.log("users", users);
-        console.log("handles", handles);
-        console.log("arr", userArray);
-        console.log("ids", ids);
+    let collect = new ListCollection({
+        listsData: data,
+        local: true
+    });
+    collect.loaded.then(c => {
+        console.log(c.lists);
+        const combos = getCombinations(Array.from(c.lists.keys()));
+        const matrix = combos.reduce((m, combo) => {
+            m.push(c.findUnions(combo));
+            return m;
+        }, []);
+        console.log(matrix);
+        drawChart(matrix, 800, 800);
     });
 });
+
+function drawChart(data, height, width){
+    var chart = venn.VennDiagram()
+        .width(width)
+        .height(height);
+    var div = d3.select("#venn")
+    div.datum(data).call(chart);
+    var tooltip = d3.select("body").append("div")
+    .attr("class", "venntooltip");
+
+    div.selectAll("path")
+    .style("stroke-opacity", 0)
+    .style("stroke", "#fff")
+    .style("stroke-width", 3)
+    
+    div.selectAll("g")
+    .on("mouseover", function(d, i) {
+        // sort all the areas relative to the current item
+        venn.sortAreas(div, d);
+        // Display a tooltip with the current size
+        tooltip.transition().duration(400).style("opacity", .9);
+        tooltip.text(d.size + " users");
+        // highlight the current path
+        var selection = d3.select(this).transition("tooltip").duration(400);
+        selection.select("path")
+            .style("fill-opacity", d.sets.length == 1 ? .4 : .1)
+            .style("stroke-opacity", 1);
+    })
+    .on("mousemove", function() {
+        tooltip.style("left", (d3.event.pageX) + "px")
+            .style("top", (d3.event.pageY - 28) + "px");
+    })
+    .on("mouseout", function(d, i) {
+        tooltip.transition().duration(400).style("opacity", 0);
+        var selection = d3.select(this).transition("tooltip").duration(400);
+        selection.select("path")
+            .style("fill-opacity", d.sets.length == 1 ? .25 : .0)
+            .style("stroke-opacity", 0);
+    });
+}
